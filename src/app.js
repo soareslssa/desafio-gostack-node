60b74fb9e425ee2f19e786689bdf4de50ee613da
soareslssa <soareslssa.dev@gmail.com>
2021-07-06 01:36:54 -0300

    implentada rota post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -15,7 +15,20 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  /**
+   * A rota deve receber title, url e techs dentro do corpo da requisição, 
+   * sendo a URL o link para o github desse repositório. Ao cadastrar um novo
+   *  projeto, ele deve ser armazenado dentro de um objeto no seguinte formato: 
+   * { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', 
+   * techs: ["Node.js", "..."], likes: 0 }; Certifique-se que o ID seja um UUID,
+   *  e de sempre iniciar os likes como 0.
+   */
+  const {title,url,techs} = request.body;
+  const project = {id: uuid(), title, url, techs:["Node.js", "React"], likes:0};
+  repositories.push(url);
+  
+  return response.json(project);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
