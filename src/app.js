b5050e6c4603d738fbbfc8eae267793de6ca5483
fab.soares <fab.soares@hcpa.edu.br>
2021-07-06 09:07:15 -0300

    refatoracao - remove comentarios todo

--- src/app.js
+++ src/app.js
@@ -11,20 +11,12 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // rota que lista todos os repositorios
 
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  /**
-   * A rota deve receber title, url e techs dentro do corpo da requisição, 
-   * sendo a URL o link para o github desse repositório. Ao cadastrar um novo
-   *  projeto, ele deve ser armazenado dentro de um objeto no seguinte formato: 
-   * { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', 
-   * techs: ["Node.js", "..."], likes: 0 }; Certifique-se que o ID seja um UUID,
-   *  e de sempre iniciar os likes como 0.
-   */
+
   const {title,url,techs} = request.body;
   const repository = {id: uuid(), title, url, techs, likes: 0};
   repositories.push(repository);
@@ -34,8 +26,6 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  /** A rota deve alterar apenas o title, a url e as techs do repositório 
-   * que possua o id igual ao id presente nos parâmetros da rota; */
 
   const {title,url, techs} = request.body;
   const {id} = request.params;
@@ -69,13 +59,8 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
+// cria novo like
 app.post("/repositories/:id/like", (request, response) => {
-  /**
-   *  A rota deve aumentar o número de likes do repositório específico 
-   * escolhido através do id presente 
-   * nos parâmetros da rota, a cada chamada dessa rota, o número de likes 
-   * deve ser aumentado em 1;
-   */
 
   const {id} = request.params;
 
@@ -88,10 +73,8 @@ app.post("/repositories/:id/like", (request, response) => {
   return response.json(repositories[repositoryIndex]);
 });
 
+// adiciona nova tech
 app.post("/repositories/:id/tech", (request, response) => {
-  /**
-   *  A rota deve adicionar uma nova tech mantem as techs do repository
-   */
 
   const {id} = request.params;
   const {tech} = request.body;
